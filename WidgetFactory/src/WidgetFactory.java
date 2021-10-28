1f074f14bf35a51d9bff471fb570128d47bd491c
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-10-28 13:19:53 -0400

    WidgetFactory Complete

--- WidgetFactory/src/WidgetFactory.java
+++ WidgetFactory/src/WidgetFactory.java
@@ -35,19 +35,40 @@ public class WidgetFactory {
     public static void main(String[] args) {
         int numDays = Integer.parseInt(JOptionPane.showInputDialog("How many days?"));
 
-
         double productionCost = productionCost(numDays);
         double profit = profit(numDays, productionCost);
+        int widgetsADay = numWidgets(numDays);
+
+        message(widgetsADay, numDays, productionCost, profit);
+
+        }
+
+        public static void message(int widgetsADay, int numDays, double productionCost, double profit){
+            DecimalFormat round = new DecimalFormat("$0.00");
+
+                String widgetsMessage = "\nNumber of widgets made are: " +  widgetsADay ;
+                String numDaysMessage = "\nNumber of days: " + numDays;
+                String widgetsCost = "\nThe cost for all the widgets is: " + round.format(widgetsADay * 10);
+                String produceCostMessage = "\nThe total cost of production is: " + round.format(productionCost);
+                String profitsMessage = "\nTotal profits are: " + round.format(profit);
+
+            JOptionPane.showMessageDialog(null, widgetsMessage + numDaysMessage + widgetsCost + produceCostMessage + profitsMessage);
+
         }
 
         public static double productionCost (int numDays){
 
-            return numDays * ((2 * 8) * (5 * 16.50)) ;
+            return (2 * 8) * (5 * 16.50) * numDays ;
         }
 
         public static double profit (int numDays, double productionCost){
 
-            return  numDays * (10 * (10 * (2 * 8))) - productionCost;
+            return  (10 * (10 * (2 * 8))) * numDays - productionCost;
+        }
+
+        public static int numWidgets(int numDays){
+
+        return 10 * 2 * 8 * numDays;
         }
 
 }
