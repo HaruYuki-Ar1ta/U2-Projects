2318401671167cdc11c801b041b3a531e8ecaab5
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-10-29 13:21:52 -0400

    character 1/?

--- RPGCharacterGUI/src/RPGCharacter.java
+++ RPGCharacterGUI/src/RPGCharacter.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class RPGCharacter {
 
     /*
@@ -5,4 +8,7 @@ public class RPGCharacter {
     should display the character's battle stats. The GUI should have at least 4 label, 4 inputs and a button to click which calculates and displays
     the battle stats.
      */
+    public static void main(String[] args) {
+        new RPGCharacterGUI();
+    }
 }
--- RPGCharacterGUI/src/RPGCharacterGUI.java
+++ RPGCharacterGUI/src/RPGCharacterGUI.java
@@ -1,2 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class RPGCharacterGUI {
-}
+    /*
+    4 label, 4 inputs and a button to click which calculates and displays
+    the battle stats.
+     */
+
+    JFrame window = new JFrame("Character Sheet");
+    JPanel panel = new JPanel();
+    JButton button1 = new JButton(" button");
+    JButton button2 = new JButton(" button");
+    JButton button3 = new JButton(" button");
+    JButton button4 = new JButton(" button");
+    JLabel label1 =  new JLabel("Strength");
+    JTextArea field1 = new JTextArea();
+
+    public RPGCharacterGUI() {
+        window.setSize(500, 250);
+        window.setLocationRelativeTo(null);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel.setBackground(Color.black);
+
+        panel.add(field1);
+        panel.add(label1);
+        panel.add(button1);
+        window.add(panel);
+
+        window.setVisible(true);
+    }
+
+
+    }
+
+
+
+
+
+
